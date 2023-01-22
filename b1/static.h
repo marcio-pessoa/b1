c27f5c12c39dfae6e37aec3e1a41e4d3fcc7ecf6
Márcio Pessoa <marcio.pessoa@gmail.com>
2023-01-22 17:52:01 -0300

    refactor: rename variable

--- b1/static.h
+++ b1/static.h
@@ -11,12 +11,13 @@
 /// @param gyro_m angle speed of Y-axis
 /// @param kalman1 tilt calculation
 /// @param angleY_one
-/// @param dt tilt calculation
+/// @param samplingTime tilt calculation
 /// @return
 float Yiorderfilter(float angle_m, float gyro_m,
-                    float kalman1, float angleY_one, float dt)
+                    float kalman1, float angleY_one, float samplingTime)
 {
-  return kalman1 * angle_m + (1 - kalman1) * (angleY_one + gyro_m * dt);
+  return kalman1 * angle_m +
+         (1 - kalman1) * (angleY_one + gyro_m * samplingTime);
 }
 
 /// @brief angle PD
