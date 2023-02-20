64d34de7be6aaeb5d3890b90e8e9dc4d327d93a3
Márcio Pessoa <marcio.pessoa@gmail.com>
2023-02-20 00:41:09 -0300

    Refactor: Remove unused parameter

--- b1/config.h
+++ b1/config.h
@@ -55,7 +55,6 @@ float angle0 = 0;  // mechanical balance angle (ideally 0 degrees)
 const double kp = 34, ki = 0, kd = 0.62;  // angle loop parameter
 const double kp_turn = 24, ki_turn = 0,
              kd_turn = 0.08;  // steering loop parameter
-const double setp0 = 0;
 
 //////////////////////////////// PI variable parameter
 int cc;
